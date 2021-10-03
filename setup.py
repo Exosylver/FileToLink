4b2463ae9a4e79e65d4785d057e4f74132a4cd99
Exosylver <36348689+Exosylver@users.noreply.github.com>
2021-10-03 17:07:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ except IOError:
 setuptools.setup(
     name="tgfilestream",
     version=__version__,
-    url="https://mau.dev/tulir/tgfilestream",
+    url="https://mau.dev/tulir",
 
-    author="Tulir Asokan",
-    author_email="tulir@maunium.net",
+    author="Tulir A.",
 
     description="A Telegram bot that can stream Telegram files to users over HTTP.",
     long_description=long_desc,
